43818b8ad7811ace8fde42396fffcb14771a460e
Alex Erdei <mralexerdei@yahoo.co.uk>
2024-02-21 14:22:33 +0000

    Optimise simulation

--- src/MD.java
+++ src/MD.java
@@ -4,15 +4,17 @@ import java.awt.event.WindowEvent;
 import java.awt.image.BufferedImage;
 
 public class MD extends Canvas implements Runnable {
-    int N = 50; // number of molecules
+    int N = 1000; // number of molecules
     int canvasWidth = 400; // canvas width (and height) in pixels
-    int pixelsPerUnit = 20; // number of pixels in one distance unit
+    int pixelsPerUnit = 3; // number of pixels in one distance unit
     double boxWidth = canvasWidth * 1.0 / pixelsPerUnit;
     //We use double buffering to stop the flickering of the animation
     BufferedImage bf = new BufferedImage(canvasWidth, canvasWidth, BufferedImage.TYPE_INT_RGB);
 
     double[] x, y, vx, vy, ax, ay;
-    double dt = 0.02;
+    double dt = 0.001;
+    double halfDt = dt / 2;
+    double halfDtSquare = dt * dt / 2;
     MD() {
         setSize(canvasWidth, canvasWidth);
         Frame pictureFrame = new Frame("Molecular Dynamics");
@@ -58,8 +60,7 @@ public class MD extends Canvas implements Runnable {
         }
 
         //Set initial velocities
-        //vx[0] = 0.1;
-        //vy[0] = 0.5;
+        vx[0] = 100;
 
         Thread simulationThread = new Thread(this);
         simulationThread.start();   //it executes the run method
@@ -67,15 +68,15 @@ public class MD extends Canvas implements Runnable {
 
     private void updatePositions() {
         for (int i = 0; i < N; i++) {
-            x[i] += vx[i] * dt + 0.5 * ax[i] * dt * dt;
-            y[i] += vy[i] * dt + 0.5 * ay[i] * dt * dt;
+            x[i] += vx[i] * dt + ax[i] * halfDtSquare;
+            y[i] += vy[i] * dt + ay[i] * halfDtSquare;
         }
     }
 
     private void updateVelocities() {
         for (int i = 0; i < N; i++) {
-            vx[i] += ax[i] * 0.5 * dt;
-            vy[i] += ay[i] * 0.5 * dt;
+            vx[i] += ax[i] * halfDt;
+            vy[i] += ay[i] * halfDt;
         }
     }
 
@@ -109,14 +110,17 @@ public class MD extends Canvas implements Runnable {
                 // compute forces between molecules i and j
                 // and add on to the accelerations of both
                 double rSquareRec = 1.0 / ((x[i] - x[j]) * (x[i] - x[j]) + (y[i] - y[j]) * (y[i] - y[j]));
-                double rToSixthRec = rSquareRec * rSquareRec * rSquareRec;
-                double rToEightRec = rToSixthRec * rSquareRec;
-                double rToFourteenthRec = rToEightRec * rToSixthRec;
-                double aPerR = 24 * (2 * rToFourteenthRec - rToEightRec);
-                ax[i] += aPerR * (x[i] - x[j]);
-                ay[i] += aPerR * (y[i] - y[j]);
-                ax[j] -= aPerR * (x[i] - x[j]);
-                ay[j] -= aPerR * (y[i] - y[j]);
+                // Take into account interaction only for close particles
+                if (rSquareRec > 0.111) {
+                    double rToSixthRec = rSquareRec * rSquareRec * rSquareRec;
+                    double rToEightRec = rToSixthRec * rSquareRec;
+                    double rToFourteenthRec = rToEightRec * rToSixthRec;
+                    double aPerR = 24 * (2 * rToFourteenthRec - rToEightRec);
+                    ax[i] += aPerR * (x[i] - x[j]);
+                    ay[i] += aPerR * (y[i] - y[j]);
+                    ax[j] -= aPerR * (x[i] - x[j]);
+                    ay[j] -= aPerR * (y[i] - y[j]);
+                }
              }
         }
     }
@@ -131,13 +135,13 @@ public class MD extends Canvas implements Runnable {
     public void run() {
         computeAccelerations();
         while(true) {
-            for (int i = 0; i < 10; i++) {
+            for (int i = 0; i < 20; i++) {
                 doStep();
             }
             //Update animation when the for loop done
             paint(this.getGraphics());
             //Make thread wait for drawing animation
-            try { Thread.sleep(30); } catch (InterruptedException e) {}
+            try { Thread.sleep(1); } catch (InterruptedException e) {}
         }
     }
 
